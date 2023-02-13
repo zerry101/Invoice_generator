b47203fb419f0e8f8d17f9e01f44003bfee5ed43
zerry101 <macone394@gmail.com>
2023-02-13 22:05:43 +0530

    added billingForm Logic

--- src/components/billing-form/billing-form.component.ts
+++ src/components/billing-form/billing-form.component.ts
@@ -1,36 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder,FormGroup} from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-billing-form',
   templateUrl: './billing-form.component.html',
   styleUrls: ['./billing-form.component.scss']
 })
-export class BillingFormComponent  implements OnInit{
+export class BillingFormComponent implements OnInit {
 
-  userForm: FormGroup=new FormGroup('');
+  userForm: FormGroup = new FormGroup('');
 
-  constructor(private fb:FormBuilder){
+  constructor(private fb: FormBuilder) {
   }
   ngOnInit(): void {
     this.setupForm();
     // throw new Error('Method not implemented.');
   }
 
-  setupForm(){
+  setupForm() {
 
-    this.userForm=this.fb.group({
-      Name:[""],
-      Address:[""],
-      GSTNO:[""],
-      productData:[""]
-
-
-
+    this.userForm = this.fb.group({
+      Name: [""],
+      Address: [""],
+      GSTNO: ["", [Validators.required, Validators.pattern('^([0][1-9]|[1-2][0-9]|[3][0-7])([a-zA-Z]{5}[0-9]{4}[a-zA-Z]{1}[1-9a-zA-Z]{1}[zZ]{1}[0-9a-zA-Z]{1})+$')]],
+      productData: this.fb.array([this.initItemRows]),
     })
+
+
   }
 
 
 
+  initItemRows(){
+    return this.fb.group({
+      index:[""],
+      Description:[""],
+      HSN:[""],
+      Quantity:[""],
+      Rate:[""]
+    })
+  }
+
+
+    get formarr(){
+      return this.userForm.get('productData') as FormArray;
+    }
+
+    addNewRow(){
+      this.formarr?.push(this.initItemRows());
+    }
+
+    deleteRow(index:number){
+      this.formarr.removeAt(index);
+    }
+
 
 
 }
+
+
+
