5d786a3e1f02ac5d1684186e6c4ff2dc21a3e66f
zerry101 <macone394@gmail.com>
2023-02-13 19:34:25 +0530

    added form

--- src/components/billing-form/billing-form.component.ts
+++ src/components/billing-form/billing-form.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup} from '@angular/forms';
 @Component({
   selector: 'app-billing-form',
   templateUrl: './billing-form.component.html',
   styleUrls: ['./billing-form.component.scss']
 })
-export class BillingFormComponent {
+export class BillingFormComponent  implements OnInit{
+
+  userForm: FormGroup=new FormGroup({});
+
+  constructor(private fb:FormBuilder){
+  }
+  ngOnInit(): void {
+    this.setupForm();
+    // throw new Error('Method not implemented.');
+  }
+
+  setupForm(){
+
+    this.userForm=this.fb.group({
+      Name:[""],
+      Address:[""],
+      GSTNO:[""],
+
+
+
+    })
+  }
+
+
+
+
 
 }
