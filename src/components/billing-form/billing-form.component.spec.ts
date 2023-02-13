0d7f52b7c933b688ce213f2091cf7a491039a0b9
zerry101 <macone394@gmail.com>
2023-02-13 18:38:49 +0530

    added billingform in components folder

---
+++ src/components/billing-form/billing-form.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BillingFormComponent } from './billing-form.component';
+
+describe('BillingFormComponent', () => {
+  let component: BillingFormComponent;
+  let fixture: ComponentFixture<BillingFormComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BillingFormComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(BillingFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/components/billing-form/billing-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-billing-form',
+  templateUrl: './billing-form.component.html',
+  styleUrls: ['./billing-form.component.scss']
+})
+export class BillingFormComponent {
+
+}
