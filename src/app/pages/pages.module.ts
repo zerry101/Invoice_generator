f2682a879e6089e52bf7f738b27a8b33320c10df
zerry101 <macone394@gmail.com>
2023-02-16 23:59:11 +0530

    added all material modules

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,16 +4,81 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { BillingFormComponent } from './billing-form/billing-form.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
 import { PagesComponent } from './pages.component';
 import { ComponentsModule } from '../components/components.module';
 import { CustomerInvoiceComponent } from './customer-invoice/customer-invoice.component';
+
+
+// import {MatCheckboxModule} from '@angular/material';
+// import {MatButtonModule} from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+const ALlMaterials=[
+  MatAutocompleteModule,
+  // MatCheckboxModule,
+  MatDatepickerModule,
+  // MatNativeDateModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatMenuModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatCardModule,
+  // MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatListModule,
+  MatStepperModule,
+  MatTabsModule,
+  // MatTreeModule,
+  // MatButtonModule,
+  MatButtonToggleModule,
+  // MatBadgeModule,
+  MatChipsModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+  MatProgressBarModule,
+  // MatRippleModule,
+  // MatBottomSheetModule,
+  MatDialogModule,
+  MatSnackBarModule,
+  MatTooltipModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatTableModule,
+]
 
 @NgModule({
   declarations: [
@@ -23,18 +88,15 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     CustomerInvoiceComponent
   ],
   imports: [
+    ALlMaterials,
     PagesRoutingModule,
     CommonModule,
-    ReactiveFormsModule,
-    MatBadgeModule,
-    MatTableModule,
-    RouterModule,
-    MatButtonModule,
-    MatIconModule,
-    FormsModule,
-    MatDialogModule,
     ComponentsModule,
-    MatFormFieldModule
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+
+
 
   ]
 })
