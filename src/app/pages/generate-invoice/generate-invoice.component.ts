b167d2b6487279a11ee8f628909121ba7f1a7720
zerry101 <macone394@gmail.com>
2023-02-25 00:11:06 +0530

    added pdf format

--- src/app/pages/generate-invoice/generate-invoice.component.ts
+++ src/app/pages/generate-invoice/generate-invoice.component.ts
@@ -8,13 +8,16 @@ import html2canvas from 'html2canvas';
 import * as pdfMake from "pdfmake/build/pdfmake";
 import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 // import * from 'html-to-pdfmake';
-
+import htmlToPdfmake from 'html-to-pdfmake';
+// import * as html2pdf from 'html2pdf.js';
 
 (<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'igx-generate-invoice',
   templateUrl: './generate-invoice.component.html',
+
+
   styleUrls: ['./generate-invoice.component.scss']
 })
 export class GenerateInvoiceComponent implements OnInit, AfterViewInit {
@@ -23,16 +26,15 @@ export class GenerateInvoiceComponent implements OnInit, AfterViewInit {
   @ViewChild('box', { static: false }) box: ElementRef | undefined;
   count = 0;
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
   constructor(public sD: SharedDataService) { }
 
   ngOnInit(): void {
     this.sD.exclusive.next(true);
     this.sD.getPrintInvoice().subscribe((res) => {
 
-      res === 'download' ? pdfMake.createPdf(this.docDefination).download() : false;    //  console.log(res);
-      res === 'print' ?     pdfMake.createPdf(this.docDefination).print():false;
-      res==='preview'?    pdfMake.createPdf(this.docDefination).open():false;
+      res === 'download' ? this.makePDF() : false;    //  console.log(res);
+      // res === 'print' ? pdfMake.createPdf(this.docDefination).print() : false;
+      // res === 'preview' ? pdfMake.createPdf(this.docDefination).open() : false;
 
     })
 
@@ -45,15 +47,27 @@ export class GenerateInvoiceComponent implements OnInit, AfterViewInit {
     // throw new Error('Method not implemented.');
     // console.log(this.box);
     console.log();
+
     // this.box?.nativeElement.style.background='blue';
   }
 
 
   makePDF() {
-    const pdf = new jsPDF('p', 'mm', 'letter');
+    // const width = this.box?.nativeElement.innerHTML.pageSize.getWidth();
+// const height = this.box?.nativeElement.innerHTML.pageSize.getHeight();
+
+// console.log(width,height);
+
+    const pdf = new jsPDF({
+      orientation: 'p',
+        unit: 'pt',
+        format: [1700,1700]
+    });
     pdf.html(this.box?.nativeElement, {
-      callback: (pdf) => { pdf.save("demo.pdf"); }
+      callback: (pdf) => { pdf.save("demo.pdf."); }
     })
+
+    // pdf.createPDF(this.box?.nativeElement).open();
   }
 
   public openPDF(): void {
@@ -73,23 +87,26 @@ export class GenerateInvoiceComponent implements OnInit, AfterViewInit {
 
   // docDefination: any;
 
-    docDefination:any = {
-      header: 'C#Corner PDF Header',
-      content: 'Sample PDF generated with Angular and PDFMake for C#Corner Blog'
-    };
+  docDefination: any = {
+    header: 'C#Corner PDF Header',
+    content: 'Sample PDF generated with Angular and PDFMake for C#Corner Blog'
+  };
 
-    // html:any=htmlToPdfmake
-
-
+  // html:any=htmlToPdfmakeht
 
 
 
+  generatePDF(){ new Promise((resolve, reject) => {
+    // var inclusions = document.getElementById('inclusions');
+    html2canvas(this.box?.nativeElement.innerHTML).then((canvas) => {
+        this.box?.nativeElement.appendChild(canvas);
+        const data_1 = canvas.toDataURL();
+        resolve(data_1);
+        console.log(this.box);
+    });
+});
+
+
+
+  }
 }
-
-
-
-  // increasecount(){
-  // this.count++;
-  // }
-
-
