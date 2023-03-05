c684c185818ff41440fef5973514ae85ee8ebcea
zerry101 <macone394@gmail.com>
2023-03-05 17:28:07 +0530

    added mat table

--- src/app/pages/customer-invoice/customer-invoice.component.ts
+++ src/app/pages/customer-invoice/customer-invoice.component.ts
@@ -12,30 +12,51 @@ import {MatTableModule} from '@angular/material/table';
   styleUrls: ['./customer-invoice.component.scss']
 })
 export class CustomerInvoiceComponent implements OnInit,AfterViewInit {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  ELEMENT_DATA: Array<Object> = [];
   constructor(private router: Router, public dt: DataTransferService) {
+    // throw new Error('Method not implemented.');
+
+    console.log('this is element data');
   }
 
 
   ngOnInit(): void {
     this.fetchData();
-    // throw new Error('Method not implemented.');
+
+
   }
 
-  Data:any;
+  Data!:any;
 
-  fetchData(){
+  fetchData():void{
    this.Data=this.dt.data().subscribe((dataObj:any)=>{
       console.log(dataObj);
+      console.log(typeof(dataObj));
+      console.log('email id type');
+
+      console.log(dataObj[0].email_id);
+
+      this.ELEMENT_DATA=dataObj;
+      this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+      console.log('this is ele data');
+      console.log(this.ELEMENT_DATA);
+
 
    })
   }
 
 
 
+  // eslint-disable-next-line @typescript-eslint/ban-types
 
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+
+
+  displayedColumns: string[] = ['id','lastname' , 'firstname','email_id' ];
+
+  dataSource!:any;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator ;
 
@@ -45,31 +66,10 @@ export class CustomerInvoiceComponent implements OnInit,AfterViewInit {
   }
 
 
+   // eslint-disable-next-line @typescript-eslint/ban-types
 
 
 }
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-const ELEMENT_DATA: Array<Object> = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl'},
-  {position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar'},
-  {position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K'},
-  {position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca'},
-];
 
